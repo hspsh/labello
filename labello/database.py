20099ebb012c7acfadbdcb6d0a89ecb221ddb73d
Norbert Szulc <norbert@not7cd.net>
2021-03-02 15:28:28 +0100

    Add database

---
+++ labello/database.py
@@ -0,0 +1,27 @@
+import os
+from datetime import datetime, timedelta
+
+import peewee as pw
+
+db = pw.SqliteDatabase(os.environ.get("DB_PATH", "labello.db"))
+
+class Label(pw.Model):
+    id = pw.PrimaryKeyField()
+    raw = pw.CharField(null=True)
+    last_edit = pw.DateTimeField()
+
+example = """
+N
+q812
+S2
+A50,0,0,1,1,1,N,"Example 1 0123456789"
+A50,50,0,2,1,1,N,"Example 2 0123456789"
+A50,100,0,3,1,1,N,"Example 3 0123456789"
+A50,150,0,4,1,1,N,"Example 4 0123456789"
+A50,200,0,5,1,1,N,"EXAMPLE 5 0123456789"
+A50,300,0,3,2,2,R,"Example 6 0123456789"
+LO25,600,750,20
+B50,800,0,3,3,7,200,B,"998152-001"
+P1
+
+"""
--- labello/web.py
+++ labello/web.py
@@ -17,6 +17,7 @@ from flask import (
 )
 
 from labello import settings
+from labello.database import db, Label
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -28,6 +29,17 @@ app.secret_key = 69420
 
 common_vars_tpl = {"version": __version__, "site_name": settings.name, "base_url": settings.base_url}
 
+@app.before_request
+def before_request():
+    app.logger.debug("connecting to db")
+    db.connect()
+
+
+@app.teardown_appcontext
+def after_request(error):
+    app.logger.debug("closing db")
+    db.close()
+
 def send_raw_to_printer(data, printer):
     with tempfile.NamedTemporaryFile(delete=False, suffix='.epl') as fp:
         fp.write(data.encode())
@@ -37,8 +49,28 @@ def send_raw_to_printer(data, printer):
     res = subprocess.call(command, shell = True)
     return res
 
+@app.route("/")
+def gallery():
+    labels = Label.select()
+    return render_template(
+        "label_gallery.html",
+        labels=labels,
+        **common_vars_tpl
+    )
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/editor", methods=["GET", "POST"])
+def send_raw():
+    """Edit or create labels"""
+    if request.method == "POST" and request.values.get("raw"):
+        data = request.values.get("raw")
+        _id = request.values.get("id", None)
+
+    return render_template(
+        "editor.html",
+        **common_vars_tpl
+    )
+
+@app.route("/send_raw", methods=["GET", "POST"])
 def send_raw():
     """Send raw text to printer"""
     if request.method == "POST" and request.values.get("raw"):
