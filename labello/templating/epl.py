0068419a04821970da1e3aa06ef59b664c37e0dd
Norbert Szulc <norbert@not7cd.net>
2023-01-10 11:43:07 +0100

    More refactoring
    This refactored version of the function is broken up into several smaller, single-purpose functions:
    
        generate_qr_code: takes a data and optional box_size and border arguments, and returns a QR code image.
        get_pixels_as_bits: takes an image and returns an iterator over its pixels, each represented as a string of '0' and '1' characters.
        bits_to_bytes: takes a string of '0' and '1' characters and returns the corresponding bytes.
        qr: takes the x, y, data and the optional box_size and border arguments, generates the QR code image, converts the image's pixels to bits and bytes, and finally formats the result and returns it.
    
    This version of the function should be more readable, as each function has a clear, easy-to-understand purpose, and it makes it easier to test each step of the process separately.

--- labello/templating/epl.py
+++ labello/templating/epl.py
@@ -1,15 +1,10 @@
 """
 Macros for EPL2 commands
 """
-
 import qrcode
 import math
 
-
-import qrcode
-import math
-
-def qr(x, y, data, box_size=3, border=0):
+def generate_qr_code(data, box_size=3, border=0):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -18,16 +13,25 @@ def qr(x, y, data, box_size=3, border=0):
     )
     qr.add_data(data)
     qr.make(fit=True)
-    img = qr.make_image()
+    return qr.make_image()
+
+def get_pixels_as_bits(img):
     pixels = img.getdata()
     width, height = img.size
     width_bytes = math.ceil(width / 8) * 8
-
-    result = f"GW{x},{y},{width_bytes // 8},{height}\n"
     for row in range(height):
-        bits = ''.join('1' if pixel else '0' for pixel in pixels[row*width : (row+1)*width]).ljust(width_bytes, '1')
-        bytes = bytes([int(bits[i:i+8], 2) for i in range(0, len(bits), 8)])
-        result += bytes.decode()
+        yield ''.join('1' if pixel else '0' for pixel in pixels[row*width : (row+1)*width]).ljust(width_bytes, '1')
+
+def bits_to_bytes(bits):
+    return bytes([int(bits[i:i+8], 2) for i in range(0, len(bits), 8)])
+
+def qr(x, y, data, box_size=3, border=0):
+    img = generate_qr_code(data, box_size, border)
+    bits = '\n'.join(get_pixels_as_bits(img))
+    bytes_ = bits_to_bytes(bits)
+
+    result = f"GW{x},{y},{len(bytes_)},{img.size[1]}\n"
+    result += bytes_.decode()
     result += '\n'
 
     return result.decode("ISO-8859-1")
