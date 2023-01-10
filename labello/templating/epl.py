5a6325eb324c44a832e72e5562a4c281463f2a1a
Norbert Szulc <norbert@not7cd.net>
2023-01-10 11:40:52 +0100

    Refactor QR code generation to EPL GW
    This version has some changes:
    
        update _qr to qr
        Using math.ceil instead of ceil
        Using f-strings to format string instead of using .format() method
        bytearray is not needed since the bytes class accepts the same arguments
        In the for-loop f = lambda b: "0" if b == 0 else "1" is replaced by a more pythonic '1' if pixel else '0'
        result += ba.decode("ISO-8859-1") is replace by result += bytes.decode() since bytes class already represents bytes and .decode() will default to utf-8 decoding

--- labello/templating/epl.py
+++ labello/templating/epl.py
@@ -3,31 +3,31 @@ Macros for EPL2 commands
 """
 
 import qrcode
-from math import ceil
+import math
 
+
+import qrcode
+import math
 
 def qr(x, y, data, box_size=3, border=0):
-    _qr = qrcode.QRCode(
+    qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=box_size,
         border=border,
     )
-    _qr.add_data(data)
-    _qr.make(fit=True)
-    img = _qr.make_image()
-    # img.show()
-    pixels = list(img.getdata())
-    w, h = img.size
-    wb = ceil(w / 8) * 8
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image()
+    pixels = img.getdata()
+    width, height = img.size
+    width_bytes = math.ceil(width / 8) * 8
 
-    result = "GW{},{},{},{}\n".format(x, y, wb // 8, h).encode()
-    for row in range(h):
-        f = lambda b: "0" if b == 0 else "1"
-        offset = row * w
-        bits = "".join([f(b) for b in pixels[offset : offset + w]]).ljust(wb, "1")
-        ba = bytearray([int(bits[i : i + 8], 2) for i in range(0, len(bits), 8)])
-        result += ba
-    result += b"\n"
+    result = f"GW{x},{y},{width_bytes // 8},{height}\n"
+    for row in range(height):
+        bits = ''.join('1' if pixel else '0' for pixel in pixels[row*width : (row+1)*width]).ljust(width_bytes, '1')
+        bytes = bytes([int(bits[i:i+8], 2) for i in range(0, len(bits), 8)])
+        result += bytes.decode()
+    result += '\n'
 
     return result.decode("ISO-8859-1")
